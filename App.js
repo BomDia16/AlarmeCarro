9827df5c01e3ca5b187f52aa92b401ad80862081
Artur <arturbarbosaoliveira@gmail.com>
2024-01-18 19:39:57 -0300

    Dia 1

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Alarme Carro!</Text>
-      <Text>Um app que notifica o usuário de um eventual roubo.</Text>
+    <View style={styles.container}>      
       <StatusBar style="auto" />
+      <View style={styles.texto}>
+        <Text>Alarme Carro!</Text>
+        <Text>Um app que notifica o usuário de um eventual roubo.</Text>
+      </View>
+
+      <Separator/>
+      <View style={styles.botoes}>
+        <Button
+          title='Adicionar carro'
+        />
+        <Button
+          title='Localizar carro'
+        />
+      </View>
     </View>
   );
 }
@@ -17,5 +31,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  texto: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  botoes: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
